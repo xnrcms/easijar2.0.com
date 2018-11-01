9c8a6e5dbead82f3a0dcce2500654364ad1f43ef
562909771 <562909771@qq.com>
2018-11-01 17:39:09 +0800

    接口开发2018-11-01

--- catalog/controller/crontab/crontab.php
+++ catalog/controller/crontab/crontab.php
@@ -1,17 +1,28 @@
 <?php
 class ControllerCrontabCrontab extends Controller {
 
-	//订单支付
+	//定时任务
 	public function index() 
 	{	
+        wr("\n==========任务执行开始" . date('Y-m-d H:i:s',time()) . "==========",'crontab.txt',false);
+
+
 		//订单超时处理
-        $this->orderTimeoutProcessing();        
+        $this->orderTimeoutProcessing();
+
+
+
+        wr("\n==========任务执行结束" . date('Y-m-d H:i:s',time()) . "==========",'crontab.txt');
     }
 
     private function orderTimeoutProcessing()
     {
-        if($this->executionFrequency(10,'orderTimeoutProcessing')){
-            wr("\n====" . date('Y-m-d H:i:s',time()) . "===\n");
+        if($this->executionFrequency(600,'orderTimeoutProcessing'))
+        {
+            //处理超出48小时未付款的订单 标记为已取消
+            $this->db->query("UPDATE `" . DB_PREFIX . "ms_suborder` SET `order_status_id`= '7' WHERE `order_status_id` <= 1 AND `order_id` IN (SELECT `order_id` FROM `" . DB_PREFIX . "order` WHERE TIMESTAMPADD(HOUR, 48, date_added) < NOW() AND `order_status_id` <= 1)");
+
+            wr("\n==========订单超时处理完成：" . date('Y-m-d H:i:s',time()) . "==========\n",'crontab.txt');
         }
         return;
     }
--- crontab.sh
+++ crontab.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 step=5 #间隔的秒数，不能大于60
-for (( i = 0; i < 60; i=(i+step) )); do
-    curl http://v2.easijar.com/index.php?route=crontab/crontab #调用链接
-    sleep $step
+
+for i in `seq 0 $step 55`
+do
+	curl http://v2.easijar.com/index.php?route=crontab/crontab #调用链接
+    if [ "$i" -ne "55" ]; then
+    	sleep $step
+    fi
 done
-exit 0
