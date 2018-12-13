c9b02edbf92ec98677463949dc28078a8fdf4be3
562909771 <562909771@qq.com>
2018-12-13 19:44:31 +0800

    接口开发-2018-12-13

--- catalog/model/extension/total/coupon.php
+++ catalog/model/extension/total/coupon.php
@@ -6,7 +6,7 @@ class ModelExtensionTotalCoupon extends Model {
 		$coupon_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "coupon` WHERE code = '" . $this->db->escape($code) . "' AND ((date_start = '0000-00-00' OR date_start < NOW()) AND (date_end = '0000-00-00' OR date_end > NOW())) AND status = '1'");
 
 		if ($coupon_query->num_rows) {
-			if ($coupon_query->row['total'] > $this->catr->getSellerTotal($coupon_query->row['seller_id'])) {
+			if ($coupon_query->row['total'] > $this->cart->getSellerTotal($coupon_query->row['seller_id'])) {
 				$status = false;
 			}
 
