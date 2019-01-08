0b708e6de10bbcb38c65021528d9fce423fc7124
562909771 <562909771@qq.com>
2019-01-08 15:42:53 +0800

    接口开发-2019-01-08

--- crontab.sh
+++ crontab.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 step=5 #间隔的秒数，不能大于60
 
+# for (( i = 0; i < 60; i=(i+step) )); do
+#     curl http://10.5.151.185/index.php?route=crontab/crontab #调用链接
+#     sleep $step
+# done
+# exit 0
 for i in `seq 0 $step 55`
 do
-	curl http://v2.easijar.com/index.php?route=crontab/crontab #调用链接
+	curl http://10.5.151.185/index.php?route=crontab/crontab #调用链接
     if [ "$i" -ne "55" ]; then
     	sleep $step
     fi
