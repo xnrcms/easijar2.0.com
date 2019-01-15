f449319755e4effbb80f0740b1ee3174869c92be
562909771 <562909771@qq.com>
2019-01-15 19:51:16 +0800

    接口开发-2019-01-15

--- catalog/controller/api/product.php
+++ catalog/controller/api/product.php
@@ -175,9 +175,9 @@ class ControllerApiProduct extends Controller {
 			$pinfo['stock'] 		= isset($product_info['quantity']) ? (int)$product_info['quantity'] : 0;
 
 			$shuoming = '<p><br><img style="display: block; margin-left: auto; margin-right: auto;" src="../image/catalog/shuoming.png" alt="undefined" data-mce-src="../image/catalog/shuoming.png" data-mce-style="display: block; margin-left: auto; margin-right: auto;" data-mce-selected="1"></p>';
-			
-			$searchStr 				= ['../image/catalog/','http://10.5.151.185/'];
-			$replaceStr 			= [trim(base_url(),'/') . '/image/catalog/',trim(base_url(),'/')];
+
+			$searchStr 				= ['../image/catalog/'];
+			$replaceStr 			= [trim(base_url(),'/') . '/image/catalog/'];
 			$subjectStr 			= htmlspecialchars_decode($product_info['description'] . $shuoming);
 
 			$pinfo['description'] 	= str_replace($searchStr, $replaceStr,$subjectStr);
