b5d4077bd3b8fb46574969d8f03d54e528a6bdb0
562909771 <562909771@qq.com>
2019-01-24 17:10:13 +0800

    接口开发-2019-01-24

--- catalog/model/rongcloud/rongcloud.php
+++ catalog/model/rongcloud/rongcloud.php
@@ -21,7 +21,7 @@ class ModelRongcloudRongcloud extends Model {
 		$this->db->query("UPDATE " . get_tabname('customer_rongcloud') . " SET date_modified = 'NOW()" . 
 			(isset($data['rongcloud_avatar']) ? "', rongcloud_avatar = '" . $this->db->escape((string)$data['rongcloud_avatar']) : '') .
 			(isset($data['rongcloud_token']) ? "', rongcloud_token = '" . $this->db->escape((string)$data['rongcloud_token']) : '') .
-			"', validity_time = '" . (int)$data['validity_time'] . 
+			(isset($data['validity_time']) ? "', validity_time = '" . (int)$data['validity_time'] : '' ) . 
 			(isset($data['rongcloud_uid']) ? "', rongcloud_uid = '" . $this->db->escape((string)$data['rongcloud_uid']) : '') .
 			(isset($data['rongcloud_nickname']) ? "', rongcloud_nickname = '" . $this->db->escape((string)$data['rongcloud_nickname']) : '') .
 			"' WHERE customer_id = '" . (int)$data['customer_id'] . "'");
