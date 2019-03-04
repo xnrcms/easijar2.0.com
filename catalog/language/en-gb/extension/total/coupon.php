b61e3fb76c266a86cb85946182deaae81fae65af
562909771 <562909771@qq.com>
2019-03-04 14:39:13 +0800

    接口开发-2019-03-04

--- catalog/language/en-gb/extension/total/coupon.php
+++ catalog/language/en-gb/extension/total/coupon.php
@@ -5,6 +5,7 @@ $_['heading_title'] = 'Use Coupon Code';
 // Text
 $_['text_coupon']   = 'Coupon (%s)';
 $_['text_success']  = 'Success: Your coupon discount has been applied!';
+$_['text_coupon_explain']   = 'Your order of %s or more';
 
 // Entry
 $_['entry_coupon']  = 'Enter your coupon here';
--- catalog/model/extension/total/multiseller_shipping.php
+++ catalog/model/extension/total/multiseller_shipping.php
@@ -207,7 +207,7 @@ class ModelExtensionTotalMultisellerShipping extends Model {
         foreach ($checkModule as $module_id)
         {
             $setting_info       = $this->model_setting_module->getModule($module_id);
-            if (!empty($setting_info) && isset($setting['product']) && !empty($setting['product']) && in_array($product_id, $setting['product'])) {
+            if (!empty($setting_info) && isset($setting_info['product']) && !empty($setting_info['product']) && in_array($product_id, $setting_info['product'])) {
                 return true;
             }
         }
